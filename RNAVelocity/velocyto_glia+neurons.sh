318011aecc660bd128cbf64cd1940786fec5966a
Katherine Beigel <123108611+beigelk@users.noreply.github.com>
2024-02-15 15:27:39 -0500

    Update velocyto_glia+neurons.sh

--- RNAVelocity/velocyto_glia+neurons.sh
+++ RNAVelocity/velocyto_glia+neurons.sh
@@ -3,7 +3,7 @@
 velocyto run --outputfolder scRNA_data/Wild-type/velocyto_loom/ \
 --samtools-threads 16 --samtools-memory 2000 \
 --bcfile scRNA_data/Wild-type/outs/filtered_feature_bc_matrix/barcodes.tsv \
---mask /home/beigelk/references/mm38/GRCm38_rmsk.gtf \
+--mask references/mm38/GRCm38_rmsk.gtf \
 scRNA_data/Wild-type/outs/possorted_genome_bam.bam \
 references/mm38/cellranger_ref/refdata-gex-mm10-2020-A/genes/genes.gtf
 
@@ -11,6 +11,6 @@ references/mm38/cellranger_ref/refdata-gex-mm10-2020-A/genes/genes.gtf
 velocyto run --outputfolder scRNA_data/Knockout/velocyto_loom/ \
 --samtools-threads 16 --samtools-memory 2000 \
 --bcfile scRNA_data/Knockout/outs/filtered_feature_bc_matrix/barcodes.tsv \
---mask /home/beigelk/references/mm38/GRCm38_rmsk.gtf \
+--mask references/mm38/GRCm38_rmsk.gtf \
 scRNA_data/Knockout/outs/possorted_genome_bam.bam \
 references/mm38/cellranger_ref/refdata-gex-mm10-2020-A/genes/genes.gtf
