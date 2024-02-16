c19207266836f183d1b0ec298f820dddd10bfd70
Katherine Beigel <123108611+beigelk@users.noreply.github.com>
2024-02-16 10:35:42 -0500

    Update velocyto_glia+neurons.sh
    Updated path and filenames to agree with directories on OSF.io and files uploaded to GEO/SRA.

--- RNAVelocity/velocyto_glia+neurons.sh
+++ RNAVelocity/velocyto_glia+neurons.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # run velocyto for the Wild-type data
-velocyto run --outputfolder scRNA_data/Wild-type/velocyto_loom/ \
+# The input data for this analysis are available on Gene Expression Omnibus (GEO) and Sequence Read Archive (SRA) (see associated publication)
+velocyto run --outputfolder RNAVelocity/DataObjects/Wildtype/ \
 --samtools-threads 16 --samtools-memory 2000 \
---bcfile scRNA_data/Wild-type/outs/filtered_feature_bc_matrix/barcodes.tsv \
+--bcfile WT_barcodes.tsv \
 --mask references/mm38/GRCm38_rmsk.gtf \
-scRNA_data/Wild-type/outs/possorted_genome_bam.bam \
+WT_possorted_genome_bam.bam \
 references/mm38/cellranger_ref/refdata-gex-mm10-2020-A/genes/genes.gtf
 
 # run velocyto for the Knockout data
-velocyto run --outputfolder scRNA_data/Knockout/velocyto_loom/ \
+# The input data for this analysis are available on Gene Expression Omnibus (GEO) and Sequence Read Archive (SRA) (see associated publication)
+velocyto run --outputfolder RNAVelocity/DataObjects/Knockout/ \
 --samtools-threads 16 --samtools-memory 2000 \
---bcfile scRNA_data/Knockout/outs/filtered_feature_bc_matrix/barcodes.tsv \
+--bcfile KO_barcodes.tsv \
 --mask references/mm38/GRCm38_rmsk.gtf \
-scRNA_data/Knockout/outs/possorted_genome_bam.bam \
+KO_possorted_genome_bam.bam \
 references/mm38/cellranger_ref/refdata-gex-mm10-2020-A/genes/genes.gtf
